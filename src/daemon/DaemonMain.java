406ddb7a634dfce28dd73627c229a4b359605a50
JulienM <julien.margarido@gmail.com>
2016-10-05 23:04:10 +0200

    update library problems and test Daemon without sources

--- src/daemon/DaemonMain.java
+++ src/daemon/DaemonMain.java
@@ -1,7 +1,14 @@
 package daemon;
 
 import java.io.IOException;
+import java.util.List;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import nosql.mongo.MongoService;
 import server.services.MuzikFinderService;
 
 /**
@@ -20,7 +27,36 @@ public class DaemonMain {
 	
 	public void process() {
 		System.out.println("===========> Daemon de remplissage de Mongo <============");
-		service.getTracksFromAPI(NB_TRACKS_PROCESS);
+//		service.getTracksFromAPI(NB_TRACKS_PROCESS);
+		
+		MongoService mongo = new MongoService();
+		System.out.println("init ok");
+		DBCollection collection = mongo.getCollection("songs");
+		System.out.println("get collection ok");
+		List<BasicDBObject> docs = mongo.createFakeBasicDBObjects();
+		mongo.insertMany(collection, docs);
+
+		BasicDBObject before = new BasicDBObject("song", "One Sweet Day");
+		BasicDBObject after = new BasicDBObject("$set", new BasicDBObject("artist", "Mariah Carey ft. Boyz II Men"));
+		mongo.updateOne(collection, before, after);
+
+		BasicDBObject findQuery = new BasicDBObject("weeksAtOne", new BasicDBObject("$gte",10));
+		BasicDBObject orderBy = new BasicDBObject("decade", 1);
+		DBCursor cursor = mongo.findBy(collection, findQuery, orderBy);
+
+		while(cursor.hasNext()){
+			DBObject doc = cursor.next();
+			System.out.println(
+					"In the " + doc.get("decade") + ", " + doc.get("song") + 
+					" by " + doc.get("artist") + " topped the charts for " + 
+					doc.get("weeksAtOne") + " straight weeks."
+					);
+		}
+
+		mongo.dropCollection(collection);
+		System.out.println("drop ok");
+		mongo.close();
+		
 		System.out.println("=========> Fin du daemon de remplissage de Mongo <==========");
 	}
 
--- src/server/servlets/LoginServlet.java
+++ src/server/servlets/LoginServlet.java
@@ -1,19 +1,12 @@
 package server.servlets;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import nosql.mongo.MongoService;
-import sources.mongo.com.mongodb.BasicDBObject;
-import sources.mongo.com.mongodb.DBCollection;
-import sources.mongo.com.mongodb.DBCursor;
-import sources.mongo.com.mongodb.DBObject;
 
 /**
  * Servlet implementation class ContactServlet
@@ -45,36 +38,6 @@ public class LoginServlet extends HttpServlet {
 		
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
-		
-		MongoService mongo = new MongoService();
-		System.out.println("mongoservice OK");
-		DBCollection collection = mongo.getCollection("songs");
-		System.out.println("mongo collection OK");
-		
-		List<BasicDBObject> docs = mongo.createFakeBasicDBObjects();
-		mongo.insertMany(collection, docs);
-		System.out.println("mongo insert many OK");
-		
-		BasicDBObject before = new BasicDBObject("song", "One Sweet Day");
-		BasicDBObject after = new BasicDBObject("$set", new BasicDBObject("artist", "Mariah Carey ft. Boyz II Men"));
-		mongo.updateOne(collection, before, after);
-
-		BasicDBObject findQuery = new BasicDBObject("weeksAtOne", new BasicDBObject("$gte",10));
-		BasicDBObject orderBy = new BasicDBObject("decade", 1);
-		DBCursor cursor = mongo.findBy(collection, findQuery, orderBy);
-
-		while(cursor.hasNext()){
-			DBObject doc = cursor.next();
-			System.out.println(
-					"In the " + doc.get("decade") + ", " + doc.get("song") + 
-					" by " + doc.get("artist") + " topped the charts for " + 
-					doc.get("weeksAtOne") + " straight weeks."
-					);
-		}
-
-		mongo.dropCollection(collection);
-
-		mongo.close();
 		
 		if(name.equals(password)){
 			request.setAttribute("message", "Hello Mr "+name+" !");

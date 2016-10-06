85495ed0fd376717d613334a2f73d6fc5f57e114
JulienM <julien.margarido@gmail.com>
2016-10-06 15:22:09 +0200

    MongoService update

--- src/nosql/mongo/MongoService.java
+++ src/nosql/mongo/MongoService.java
@@ -6,21 +6,21 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoService {
 
 	ServerAddress serverAddress;
 	MongoCredential mongoCredential;
 	MongoClient mongoClient;
-	DB db;
+	MongoDatabase db;
 
 	public MongoService() {
 		Logger mongoLogger = Logger.getLogger( "org.mongodb.driver" );
@@ -30,38 +30,38 @@ public class MongoService {
 		serverAddress = new ServerAddress("ds049456.mlab.com", 49456);
 		mongoCredential = MongoCredential.createCredential("heroku_1dpqh3kq", "heroku_1dpqh3kq", "gv7mru79jbtgn52lrl5mg301qh".toCharArray());
 		mongoClient = new MongoClient(serverAddress, Arrays.asList(mongoCredential));
-		db = mongoClient.getDB("heroku_1dpqh3kq");
+		db = mongoClient.getDatabase("heroku_1dpqh3kq");
 	}
 
 
 	// this creates collection if not exists
-	public void insertMany(DBCollection collection, List<BasicDBObject> docs){
-		collection.insert(docs);
+	public void insertMany(MongoCollection<Document> collection, List<Document> docs){
+		collection.insertMany(docs);
 	}
 
 	// this creates collection if not exists
-	public void insertOne(DBCollection collection, BasicDBObject doc){
-		collection.insert(doc);
+	public void insertOne(MongoCollection<Document> collection, Document doc){
+		collection.insertOne(doc);
 	}
 
 	// this creates collection if not exists
-	public void updateOne(DBCollection collection, BasicDBObject before, BasicDBObject after){
-		collection.update(before, after);
+	public void updateOne(MongoCollection<Document> collection, Document before, Document after){
+		collection.updateOne(before, after);
 	}
 
-	public DBCursor findAll(DBCollection collection, BasicDBObject findQuery){
-		return collection.find();
+	public MongoCursor<Document> findAll(MongoCollection<Document> collection, Document findQuery){
+		return collection.find().iterator();
 	}
 
-	public DBCursor findBy(DBCollection collection, BasicDBObject findQuery){
-		return collection.find(findQuery);
+	public MongoCursor<Document> findBy(MongoCollection<Document> collection, Document findQuery){
+		return collection.find(findQuery).iterator();
 	}
 
-	public DBCursor findBy(DBCollection collection, BasicDBObject findQuery, BasicDBObject orderBy){
-		return collection.find(findQuery).sort(orderBy);
+	public MongoCursor<Document> findBy(MongoCollection<Document> collection, Document findQuery, Document orderBy){
+		return collection.find(findQuery).sort(orderBy).iterator();
 	}
 
-	public DBCollection getCollection(String collectionName){
+	public MongoCollection<Document> getCollection(String collectionName){
 		return db.getCollection(collectionName);
 	}
 
@@ -74,7 +74,7 @@ public class MongoService {
 		}
 	}
 
-	public boolean dropCollection(DBCollection collection){
+	public boolean dropCollection(MongoCollection<Document> collection){
 		try{
 			collection.drop();
 			return true;
@@ -88,26 +88,26 @@ public class MongoService {
 	}
 	
 	// Example
-	public List<BasicDBObject> createFakeBasicDBObjects(){
+	public List<Document> createFakeDocuments(){
 
-		BasicDBObject seventies = new BasicDBObject();
+		Document seventies = new Document();
 		seventies.put("decade", "1970s");
 		seventies.put("artist", "Debby Boone");
 		seventies.put("song", "You Light Up My Life");
 		seventies.put("weeksAtOne", 10);
 
-		BasicDBObject eighties = new BasicDBObject();
+		Document eighties = new Document();
 		eighties.put("decade", "1980s");
 		eighties.put("artist", "Olivia Newton-John");
 		eighties.put("song", "Physical");
 		eighties.put("weeksAtOne", 10);
 
-		BasicDBObject nineties = new BasicDBObject();
+		Document nineties = new Document();
 		nineties.put("decade", "1990s");
 		nineties.put("artist", "Mariah Carey");
 		nineties.put("song", "One Sweet Day");
 		nineties.put("weeksAtOne", 16);
-		List<BasicDBObject> fakeData = new ArrayList<>(3);
+		List<Document> fakeData = new ArrayList<>(3);
 		fakeData.add(seventies);
 		fakeData.add(eighties);
 		fakeData.add(nineties);
@@ -115,21 +115,20 @@ public class MongoService {
 	}
 	
 	public void fakeUse(){
-		DBCollection collection = this.getCollection("songs");
+		MongoCollection<Document> collection = this.getCollection("songs");
 		
-		List<BasicDBObject> docs = this.createFakeBasicDBObjects();
+		List<Document> docs = this.createFakeDocuments();
 		this.insertMany(collection, docs);
 
-		BasicDBObject before = new BasicDBObject("song", "One Sweet Day");
-		BasicDBObject after = new BasicDBObject("$set", new BasicDBObject("artist", "Mariah Carey ft. Boyz II Men"));
+		Document before = new Document("song", "One Sweet Day");
+		Document after = new Document("$set", new Document("artist", "Mariah Carey ft. Boyz II Men"));
 		this.updateOne(collection, before, after);
 
-		BasicDBObject findQuery = new BasicDBObject("weeksAtOne", new BasicDBObject("$gte",10));
-		BasicDBObject orderBy = new BasicDBObject("decade", 1);
-		DBCursor cursor = this.findBy(collection, findQuery, orderBy);
-
+		Document findQuery = new Document("weeksAtOne", new Document("$gte",10));
+		Document orderBy = new Document("decade", 1);
+		MongoCursor<Document> cursor = this.findBy(collection, findQuery, orderBy);
 		while(cursor.hasNext()){
-			DBObject doc = cursor.next();
+			Document doc = cursor.next();
 			System.out.println(
 					"In the " + doc.get("decade") + ", " + doc.get("song") + 
 					" by " + doc.get("artist") + " topped the charts for " + 

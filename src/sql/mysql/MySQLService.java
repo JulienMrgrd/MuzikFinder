06343f9502ea34cf19b5919b302b5844c547d8bf
JulienM <julien.margarido@gmail.com>
2016-11-10 11:23:53 +0100

    change driver name to fix a instanciation bug

--- src/sql/mysql/MySQLService.java
+++ src/sql/mysql/MySQLService.java
@@ -25,7 +25,7 @@ public class MySQLService {
 	    String username = dbUri.getUserInfo().split(":")[0];
 	    String password = dbUri.getUserInfo().split(":")[1];
 	    String dbUrl = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath();
-	    Class.forName("com.mysql.cj.jdbc.Driver");
+	    Class.forName("com.mysql.jdbc.Driver");
 	    DriverManager.setLoginTimeout(10);
 		connection = DriverManager.getConnection(dbUrl, username, password);
 		connection.setAutoCommit(true);
